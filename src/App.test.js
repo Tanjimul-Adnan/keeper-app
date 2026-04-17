386fd9769b351128c69ac9c09e38340531e16139
Tanjimul-Adnan <tanjimuladnan@hotmail.com>
2026-04-17 14:06:06 +0600

    remove all files

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders Keeper title', () => {
-    render(<App />);
-    const titleElement = screen.getByText(/Keeper/i); // Match the "Keeper" title from your app
-    expect(titleElement).toBeInTheDocument();
-});
